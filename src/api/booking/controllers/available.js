742dff11ce2873c050c1b0899ed020188768dff7
Tawanda <tawandagdmavondo@gmail.com>
2022-03-11 21:22:29 +0200

    .

--- src/api/booking/controllers/available.js
+++ src/api/booking/controllers/available.js
@@ -7,14 +7,14 @@
 module.exports = {
   index: async (ctx, next) => {
     try {
-      ctx.body = "ok";
-      // console.log(ctx.request.body);
       const { checkIn, checkOut, room } = ctx.request.body;
 
       const response = await strapi
         .service("api::booking.booking")
         .checkAvailability(checkIn, checkOut, room);
-      console.log(response);
+      ctx.body = {
+        isAvailable: response,
+      };
     } catch (err) {
       ctx.body = err;
     }
