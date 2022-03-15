b55b3d48f4197bc35063c40353a914fbc085e154
Tawanda <tawandagdmavondo@gmail.com>
2022-03-15 22:39:02 +0200

    .

--- config/database.js
+++ config/database.js
@@ -1,16 +1,25 @@
 const path = require("path");
 
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: env("DATABASE_CLIENT", "mysql"),
+//     connection: {
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "strapi"),
+//       user: env("DATABASE_USERNAME", "strapi"),
+//       password: env("DATABASE_PASSWORD", "strapi"),
+//       schema: env("DATABASE_SCHEMA", ""),
+//       ssl: true,
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
 module.exports = ({ env }) => ({
   connection: {
-    client: env("DATABASE_CLIENT", "mysql"),
+    client: 'sqlite',
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
-      schema: env("DATABASE_SCHEMA", ""),
-      ssl: true,
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
     useNullAsDefault: true,
   },
